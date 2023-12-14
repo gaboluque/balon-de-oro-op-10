64da8338d6dc2549b7c88b6b09eb34b70309337e
Gabriel Luque <gluquemiranda@gmail.com>
2023-12-13 22:53:35 -0500

    relative path

--- helpers/results-repo.ts
+++ helpers/results-repo.ts
@@ -1,14 +1,15 @@
 import * as fs from "fs";
+import path from "path";
 
 
 export const getResults = () => {
-  const results = fs.readFileSync("data/results.json", "utf-8");
+  const results = fs.readFileSync(path.resolve("data/results.json"), "utf-8");
   return JSON.parse(results);
 }
 
 export const setResults = (newResults: Record<string, Record<string, number>>) => {
   const results = JSON.stringify(newResults, null, 2);
-  fs.writeFileSync("data/results.json", results);
+  fs.writeFileSync(path.resolve("data/results.json"), results);
 }
 
 export const resetResults = () => {
@@ -16,5 +17,5 @@ export const resetResults = () => {
     gb: {},
     gg: {}
   }, null, 2);
-  fs.writeFileSync("data/results.json", results);
+  fs.writeFileSync(path.resolve("data/results.json"), results);
 }
